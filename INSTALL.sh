971bbf3e5c8ac7151ddbc615f76868e99e009b97
Topic <144539876+Strykerboom@users.noreply.github.com>
2024-07-15 18:28:18 -0700

    C

--- INSTALL.sh
+++ INSTALL.sh
@@ -12,6 +12,12 @@ echo "this is the install script where you will be installing every thing that y
 
 echo "do want to continue with this install or do want Abort"
 
+# Function to display usage information
+usage() {
+    echo "Usage: $0 {install|abort}"
+    exit 1
+}
+
 # Function to install system packages using pacman
 install_system_packages() {
     yes | sudo pacman -Syu --noconfirm # Update package list and upgrade all packages
@@ -29,8 +35,6 @@ install_python_packages() {
         keras
         torch
         torchvision
-        pillow
-        sklearn
     )
 
     for package in "${python_packages[@]}"; do
